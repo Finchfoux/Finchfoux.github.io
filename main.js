4ce67843f17727a784598dfa2a2879fbfbfae9e6
Finchfoux <95510253+Finchfoux@users.noreply.github.com>
2021-12-04 17:58:41 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,40 @@
 $(document).ready(function () {
-    let image_blobs = $(".section-image");
-    // image_blobs.forEach(img => {
-    //     $.ajax({
-    //         url : img.src,
-    //         dataType: "text",
-    //         success : function (data) {
-    //             img.src = `data:image/png;base64,${data}`;
-    //         }
-    //     });
-    // });
-    for (let i = 0; i < image_blobs.length; i++) {
-        console.log(image_blobs[i].src);
-        $.ajax({
-            url : image_blobs[i].src,
-            dataType: "text",
-            success : function (data) {
-                img.src = `data:image/png;base64,${data}`;
-            }
-        });
-    }
+    var p = JSON.parse(posts);
+
+    for (let i = 0; i < p.length; i++) {
+        let main_div = document.createElement("div");
+        main_div.className = "sections";
+        main_div.id = `section-${i}`;
+
+        let _header = document.createElement("h2");
+        _header.className = "section-header";
+        _header.innerHTML = p[i].header;
+
+        console.log(_header);
+
+        let date_display = document.createElement("p");
+        date_display.style = "font-size: small;color: antiquewhite;margin-top: 0px;user-select: none; align-self: flex-end;";
+        date_display.innerHTML = p[i].date;
+
+        console.log(date_display);
+
+        let _img = document.createElement("img");
+        _img.className = "section-image";
+        _img.src = `data:image/png;base64,${p[i].image}`;
+
+        console.log(_img);
+
+        let _body = document.createElement("p");
+        _body.className = "section-text";
+        _body.innerHTML = `<strong>${p[0].body}</strong>`;
+
+        console.log(_body)
+
+        main_div.appendChild(_header);
+        main_div.appendChild(date_display);
+        main_div.appendChild(_img);
+        main_div.appendChild(_body);
+
+        document.getElementById("section-container").appendChild(main_div);
+    }    
 });
