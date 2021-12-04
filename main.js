482de85ebbeb56fa161a0d580388e34c69bc9d85
Finchfoux <95510253+Finchfoux@users.noreply.github.com>
2021-12-04 17:12:04 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-let p = JSON.parse(posts);
-
-console.log(p[0].date)
+$(document).ready(function () {
+    let image_blobs = $(".section-image");
+    // image_blobs.forEach(img => {
+    //     $.ajax({
+    //         url : img.src,
+    //         dataType: "text",
+    //         success : function (data) {
+    //             img.src = `data:image/png;base64,${data}`;
+    //         }
+    //     });
+    // });
+    for (let i = 0; i < image_blobs.length; i++) {
+        console.log(image_blobs[i].src);
+        $.ajax({
+            url : image_blobs[i].src,
+            dataType: "text",
+            success : function (data) {
+                img.src = `data:image/png;base64,${data}`;
+            }
+        });
+    }
+});
